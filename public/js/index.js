30e65ed663f03fd3f624431092a5ccab3920279a
Jesse <jesseoluwapelumi01@gmail.com>
2023-03-07 09:10:41 +0100

    added dropdown functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,26 @@
-const arrow = document.querySelectorAll(".arrow");
-const menu = document.querySelectorAll(".menu");
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   }
+  
+//   window.onclick = function(e) {
+//     if (!e.target.matches('.dropbtn')) {
+//     var myDropdown = document.getElementById("myDropdown");
+//       if (myDropdown.classList.contains('show')) {
+//         myDropdown.classList.remove('show');
+//       }
+//     }
+//   }
 
-arrow.addEventListener("click", () => {
+const dropdowns = document.querySelectorAll(".dropdown")
+dropdowns.forEach(el => {
+  el.addEventListener("click", () => {
+    const menu = el.querySelector(".menu")
+
     if (menu.classList.contains("hidden")) {
-        menu.classList.remove("hidden");
-    }
+      menu.classList.remove("hidden");
+  }else {
+    menu.classList.add("hidden")
+  }
+  })
 })
+
